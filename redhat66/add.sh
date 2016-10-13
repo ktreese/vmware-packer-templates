12d166dc0db46156725de5536847b8e5a1d126fa
Kris Reese <kris.reese@wwt.com>
2016-10-12 20:49:46 -0500

    Moves redhat 7.2 build to branch

--- redhat66/add.sh
+++
@@ -1,1 +0,0 @@
-vagrant box add --name wwt/rhel-server-6.6-x86_64 wwt-rhel-6-6-x64-virtualbox.box
--- redhat67/scripts/zerodisk.sh
+++
@@ -1,5 +0,0 @@
-# Zero out the rest of the free space using dd, then delete the written file.
-dd if=/dev/zero of=/EMPTY bs=1M
-rm -f /EMPTY
-
-sync
