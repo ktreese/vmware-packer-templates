0177a02d35e438407deb67e4e4fb0a69d5917d9f
Kris Reese <kris.reese@wwt.com>
2016-11-11 10:23:12 -0600

    Removes CentOS repo dependencies

--- scripts/base.sh
+++ scripts/base.sh
@@ -1,23 +1,17 @@
-yum -y install perl
-
 # set MOTD w/puppet console access information
 cat << MOTD > /etc/motd
- __          ___          _________   _____          _ _    _       _   
- \ \        / | \        / /__   __| |  __ \        | | |  | |     | |  
-  \ \  /\  / / \ \  /\  / /   | |    | |__) |___  __| | |__| | __ _| |_ 
-   \ \/  \/ /   \ \/  \/ /    | |    |  _  // _ \/ _\` |  __  |/ _\` | __|
-    \  /\  /     \  /\  /     | |    | | \ \  __/ (_| | |  | | (_| | |_ 
-     \/  \/       \/  \/      |_|    |_|  \_\___|\__,_|_|  |_|\__,_|\__|
-                                                                        
+
+                    _     _____          _   _    _       _     ______ 
+                   | |   |  __ \        | | | |  | |     | |   |____  |
+ __      ___      _| |_  | |__) |___  __| | | |__| | __ _| |_      / / 
+ \ \ /\ / | \ /\ / / __| |  _  // _ \/ _\` | |  __  |/ _\` | __|    / /  
+  \ V  V / \ V  V /| |_  | | \ \  __/ (_| | | |  | | (_| | |_    / /   
+   \_/\_/   \_/\_/  \__| |_|  \_\___|\__,_| |_|  |_|\__,_|\__|  /_/    
+                                                                       
+                                                                       
+
 MOTD
 
 # Set pretty prompt; borrow from STLPUG and change prompt prefix
 curl -so /etc/profile.d/prompt.sh https://gist.githubusercontent.com/ktreese/1ad2dc99aa2840b9e80d/raw/d37451ffc0f6ea54d6ba046ea661f8893744b16f/prompt.sh
 sed -i -e 's/STLPUG/WWT/g' /etc/profile.d/prompt.sh
-
-# Disable annoying RedHat subscription alert upon yum invocation
-sed -i -e 's/1/0/' /etc/yum/pluginconf.d/subscription-manager.conf
-
-# Setup use of CentOS repo for package availability; mv from tmp as packer provisioner cannot write to /etc/yum.repos.d
-mv /tmp/media.repo /etc/yum.repos.d/
-mv /tmp/CentOS-Base.repo /etc/yum.repos.d/
--- scripts/cleanup.sh
+++ scripts/cleanup.sh
@@ -1,4 +1,7 @@
-yum -y erase kernel-2.6.32-431.el6.x86_64
+if [ $(rpm -qa | grep "^kernel-[0-9]" | wc -l) -eq 2 ]; then
+  yum -y erase kernel-2.6.32-431.el6.x86_64
+  yum -y erase kernel-devel-2.6.32-431.el6.x86_64
+fi
 yum -y clean all
 rm -rf VBoxGuestAdditions_*.iso
 rm -rf /tmp/rubygems-*
