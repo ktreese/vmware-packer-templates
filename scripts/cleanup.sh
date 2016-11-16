76b8bad222d8bd50e281a384aec9f6f3e1013e22
Kris Reese <kris.reese@wwt.com>
2016-11-16 14:55:38 -0600

    Sets user variable for subscription manager password

--- scripts/cleanup.sh
+++ scripts/cleanup.sh
@@ -1,7 +1,7 @@
 # yum update is run in %post; clean up old kernel remnants
 if [ $(rpm -qa | grep "^kernel-[0-9]" | wc -l) -eq 2 ]; then
-  rpm -e kernel-2.6.32-573.el6.x86_64
-  rpm -e kernel-devel-2.6.32-573.el6.x86_64
+  rpm -e kernel-2.6.32-642.el6.x86_64
+  rpm -e kernel-devel-2.6.32-642.el6.x86_64
 fi
 yum -y clean all
 rm -rf VBoxGuestAdditions_*.iso
