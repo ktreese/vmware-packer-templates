8b016c79cec101168c2cda4d0dadb8675aaf227e
Kris Reese <kris.reese@wwt.com>
2016-11-19 21:28:09 -0600

    Adjust cleanup scripts for vmware

--- scripts/cleanup.sh
+++ scripts/cleanup.sh
@@ -3,7 +3,3 @@ if [ $(rpm -qa | grep "^kernel-[0-9]" | wc -l) -eq 2 ]; then
   rpm -e kernel-2.6.32-573.el6.x86_64
   rpm -e kernel-devel-2.6.32-573.el6.x86_64
 fi
-yum -y clean all
-rm -rf VBoxGuestAdditions_*.iso
-rm -rf /tmp/rubygems-*
-cat /dev/null > /var/log/wtmp
