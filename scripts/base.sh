56fa48b3b120b6f45cdb96740df3de4b22bbcfbd
Kris Reese <kris.reese@wwt.com>
2016-11-23 22:37:37 -0600

    Updates scripts for vmware build

--- scripts/base.sh
+++ scripts/base.sh
@@ -1,25 +1,16 @@
 # set MOTD w/puppet console access information
 cat << MOTD > /etc/motd
 
-                    _                                  _                         _            
-                   | |                                | |                       | |           
- __      ___      _| |_   _ __  _   _ _ __  _ __   ___| |_   _ __ ___   __ _ ___| |_ ___ _ __ 
- \ \ /\ / | \ /\ / / __| | '_ \| | | | '_ \| '_ \ / _ \ __| | '_ \` _ \ / _\` / __| __/ _ \ '__|
-  \ V  V / \ V  V /| |_  | |_) | |_| | |_) | |_) |  __/ |_  | | | | | | (_| \__ \ ||  __/ |   
-   \_/\_/   \_/\_/  \__| | .__/ \__,_| .__/| .__/ \___|\__| |_| |_| |_|\__,_|___/\__\___|_|   
-                         | |         | |   | |                                                
-                         |_|         |_|   |_|                                                
-
-
-Console Access:
-  https://localhost:4443
-  username: admin
-  password: wwtpuppet
+                    _                                  _                            _   
+                   | |                                | |                          | |  
+ __      ___      _| |_   _ __  _   _ _ __  _ __   ___| |_    __ _  __ _  ___ _ __ | |_ 
+ \ \ /\ / | \ /\ / / __| | '_ \| | | | '_ \| '_ \ / _ \ __|  / _\` |/ _\` |/ _ \ '_ \| __|
+  \ V  V / \ V  V /| |_  | |_) | |_| | |_) | |_) |  __/ |_  | (_| | (_| |  __/ | | | |_ 
+   \_/\_/   \_/\_/  \__| | .__/ \__,_| .__/| .__/ \___|\__|  \__,_|\__, |\___|_| |_|\__|
+                         | |         | |   | |                      __/ |               
+                         |_|         |_|   |_|                     |___/                
 
 MOTD
-
-# set puppetmaster in /etc/hosts or puppet will fail to install during invocation of puppet.sh
-sed -i -e 's/127.0.0.1   /127.0.0.1   puppetmaster puppetmaster.wwt.com puppetmaster.wwt.local /' /etc/hosts
 
 # Set pretty prompt; borrow from STLPUG and change prompt prefix
 curl -so /etc/profile.d/prompt.sh https://gist.githubusercontent.com/ktreese/1ad2dc99aa2840b9e80d/raw/d37451ffc0f6ea54d6ba046ea661f8893744b16f/prompt.sh
