dcda3e3d608730ff4080c2617836d911478cf317
Kris Reese <kris.reese@wwt.com>
2016-11-29 09:41:33 -0600

    Updates build for vmware-iso builder

--- Vagrantfile
+++ Vagrantfile
@@ -3,16 +3,17 @@ Vagrant.configure("2") do |config|
     puppetmaster.vm.box = 'vmware/dummy'
     puppetmaster.vm.box_url = './dummy.box'
     puppetmaster.vm.hostname = 'vmwpuppetmaster'
+    puppetmaster.vm.provision :shell, :inline => "sudo /bin/echo \"$(/sbin/ip route get 8.8.8.8 | awk 'NR==1 {print $NF}') $HOSTNAME\" >> /etc/hosts"
 
     puppetmaster.vm.provider :vsphere do |vsphere|
       vsphere.host = '192.168.1.10'
       vsphere.insecure = true
       vsphere.compute_resource_name = '3031'
-      vsphere.template_name = 'Templates/redhat6'
-      vsphere.name = 'vmwansible'
+      vsphere.template_name = 'Templates/puppetmaster'
+      vsphere.name = 'vmwpuppetmaster'
       vsphere.user = 'root'
       vsphere.password = ENV['SUB_PASSWORD']
-      vsphere.memory_mb = 2048
+      vsphere.memory_mb = 4096
       vsphere.cpu_count = 2
     end
   end
--- scripts/puppet.sh
+++ scripts/puppet.sh
@@ -10,6 +10,9 @@ run_puppet() {
   done
 }
 
+# set IP in /etc/hosts so puppet install succeeds:
+/bin/echo "$(/sbin/ip route get 8.8.8.8 | awk 'NR==1 {print $NF}') $HOSTNAME" >> /etc/hosts
+
 curl -o /home/vagrant/puppet-enterprise-2016.2.1-el-6-x86_64.tar.gz https://s3.amazonaws.com/pe-builds/released/2016.2.1/puppet-enterprise-2016.2.1-el-6-x86_64.tar.gz
 tar zxvf /home/vagrant/puppet-enterprise-2016.2.1-el-6-x86_64.tar.gz
 /home/vagrant/puppet-enterprise-2016.2.1-el-6-x86_64/puppet-enterprise-installer -c /tmp/puppetmaster.conf
@@ -22,36 +25,39 @@ run_puppet
 
 # Update r10k & Install ruby gems for classifications.rb (refer wwt-puppet-control production branch in scripts dir)
 /opt/puppetlabs/puppet/bin/gem update r10k
-/opt/puppetlabs/puppet/bin/gem install puppetclassify
-/opt/puppetlabs/puppet/bin/gem install mongo
+#/opt/puppetlabs/puppet/bin/gem install puppetclassify
+#/opt/puppetlabs/puppet/bin/gem install mongo
 
-# Setup access to github for root and pe-git; creates .ssh/known_hosts
-/usr/bin/ssh -oStrictHostKeyChecking=no github.wwt.com
-/bin/su -lc "ssh -oStrictHostKeyChecking=no github.wwt.com" pe-git
+## Setup access to github for root and pe-git; creates .ssh/known_hosts
+#/usr/bin/ssh -oStrictHostKeyChecking=no github.wwt.com
+#/bin/su -lc "ssh -oStrictHostKeyChecking=no github.wwt.com" pe-git
 
-# Copy github keys into pe-git home dir; referenced by r10k.yaml
-/bin/mv /tmp/id_rsa* /home/pe-git/.ssh
-/bin/chmod 700 /home/pe-git/.ssh/id_rsa
+## Copy github keys into pe-git home dir; referenced by r10k.yaml
+#/bin/mv /tmp/id_rsa* /home/pe-git/.ssh
+#/bin/chmod 700 /home/pe-git/.ssh/id_rsa
 
-# Copy temp r10k.yaml in order to deploy production; subsequent puppet run will overwrite via pe_r10k classification
-/bin/mv /tmp/r10k.yaml /etc/puppetlabs/r10k/r10k.yaml
-/bin/mv /tmp/hiera.yaml /etc/puppetlabs/puppet/hiera.yaml
+## Copy temp r10k.yaml in order to deploy production; subsequent puppet run will overwrite via pe_r10k classification
+#/bin/mv /tmp/r10k.yaml /etc/puppetlabs/r10k/r10k.yaml
+#/bin/mv /tmp/hiera.yaml /etc/puppetlabs/puppet/hiera.yaml
 
-# Restart pe-puppetserver for hiera update to take effect
-/sbin/service pe-puppetserver restart
+## Restart pe-puppetserver for hiera update to take effect
+#/sbin/service pe-puppetserver restart
 
-# Deploy production and vgt environments to puppetmaster
-/usr/local/bin/r10k deploy environment production -pv -c /etc/puppetlabs/r10k/r10k.yaml
+## Deploy production and vgt environments to puppetmaster
+#/usr/local/bin/r10k deploy environment production -pv -c /etc/puppetlabs/r10k/r10k.yaml
 
-# Import classifications from MongoDB
-/opt/puppetlabs/puppet/bin/ruby /etc/puppetlabs/code/environments/production/scripts/classifications.rb -i
+## Import classifications from MongoDB
+#/opt/puppetlabs/puppet/bin/ruby /etc/puppetlabs/code/environments/production/scripts/classifications.rb -i
 
 # Invoke 2nd puppet run to setup hiera, the pe_repos, and tune JVM for low memory vagrant VM
 run_puppet
 
-# profile::wwt_hiera generates keys upon force puppet run above; trash these
-# copy insecure vagrant eyaml private/public keys
-/bin/mv -f /tmp/*.pem /etc/puppetlabs/puppet/keys/
+## profile::wwt_hiera generates keys upon force puppet run above; trash these
+## copy insecure vagrant eyaml private/public keys
+#/bin/mv -f /tmp/*.pem /etc/puppetlabs/puppet/keys/
 
 # Set PATH
 /bin/sed -i '/^PATH/c PATH=\$PATH:/opt/puppetlabs/puppet/bin:/etc/puppetlabs/code/environments/production/scripts:/opt/puppetlabs/bin:/vagrant/files/' /root/.bash_profile
+
+# Remove added hosts entry
+/bin/sed -i -e "/$HOSTNAME/d" /etc/hosts
