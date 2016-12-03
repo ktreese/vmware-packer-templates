60cca3e568cd25190b2c1f1d1d2e9e32bdd64118
Kris Reese <kris.reese@wwt.com>
2016-12-03 14:56:10 -0600

    open-vm-tools on RHEL6 requires EPEL

--- scripts/redhat-vmware-tools_install.sh
+++ scripts/redhat-vmware-tools_install.sh
@@ -1,2 +1,10 @@
 #!/bin/bash
-yum -y install open-vm-tools
+
+# open-vm-tools are not supported on RHEL 6; however, this package is available from the EPEL repository.
+/usr/bin/curl -ko /tmp/epel.rpm http://dl.fedoraproject.org/pub/epel/epel-release-latest-6.noarch.rpm
+/bin/rpm -ivh /tmp/epel.rpm
+/usr/bin/yum -y install open-vm-tools
+
+# clean up
+/bin/rpm -e epel-release
+rm -f /tmp/epel.rpm
